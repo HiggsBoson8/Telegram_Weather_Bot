bfeeeb05dcfc96d3e46de4c31f437c32084abe97
Abay <120572718+HiggsBoson8@users.noreply.github.com>
2023-02-02 11:03:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ created: {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
         message_count.append(random.randint(0, 1))
         await message.reply(get_weather_def)
 
-        if len(message_count) >= 3:
+        if len(message_count) >= 4:
             await message.answer("Ну все, хватит спамить!")
             await message.reply_sticker(random.choice(STICKER_001))
             message_count.clear()
